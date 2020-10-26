fbec45a69462001f74d97171cd9306321b585ca7
mcfarkar <mcfarkar@oregonstate.edu>
2020-10-26 04:01:37 -0700

    Implemented User being able to view the last 20 posts submitted Implemented User being able to switch between different tabs - viewing all posts (feed view), compose (capture photos form camera) and profile tabs (posts made) using fragments and a Bottom Navigation View.

---
+++ app/src/main/java/com/codepath/mcfarkar/kinsta/PostsAdapter.java
@@ -0,0 +1,92 @@
+package com.codepath.mcfarkar.kinsta;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+/**
+ * Created by mcfarkar on 26,October,2020
+ */
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+// 3. Added PostsAdapter.Viewholder above
+
+// 4 Create 2 parameters needed for constructor
+    private Context context;
+    private List<Post> posts;
+
+// 5 create a constructor
+    public PostsAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // 7 create layout inflator which will return a view
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // 8 get post located at this position
+        Post post = posts.get(position);
+        holder.bind(post);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        // 6 follows from constructor
+        return posts.size();
+    }
+
+    // 1 need to define a viewholder before updating function header to reference vh
+    class ViewHolder extends  RecyclerView.ViewHolder{
+        // 2 Add a constructor
+
+        // 10 get references need for bind function
+        private TextView tvUsername;
+        private ImageView ivImage;
+        private TextView  tvDescription;
+
+        public ViewHolder(@NonNull View itemView)
+        {
+
+
+            super(itemView);
+
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+
+        }
+
+        public void bind(Post post) {
+            // 9 Bind the post data into the view elements
+            tvDescription.setText(post.getDescription());
+            tvUsername.setText(post.getUser().getUsername());
+            ParseFile image = post.getImage();
+            if(image != null) {
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/codepath/mcfarkar/kinsta/fragments/PostsFragment.java
@@ -0,0 +1,105 @@
+package com.codepath.mcfarkar.kinsta.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.mcfarkar.kinsta.Post;
+import com.codepath.mcfarkar.kinsta.PostsAdapter;
+import com.codepath.mcfarkar.kinsta.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PostsFragment extends Fragment {
+
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    private PostsAdapter adapter;
+    private List<Post> allPosts;
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(),allPosts );
+
+
+//        How to Use the Recycler View
+//        0 Create layout for one row in the list
+
+
+//        1 Create the adapter:
+//              a. PostsAdapter class
+//              b. line above
+
+//        2 Create the data source: already done previously
+
+//        3 Set the adapter on the Recycler View
+        rvPosts.setAdapter(adapter);
+
+//        4 Set the Layout Manager on the Recycler View
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        queryPosts();
+
+    }
+
+    private void queryPosts() {
+
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+
+                if (e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                // if query succeeded, iterate through all of the posts
+                for (Post post : posts){
+                   Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+//                    Log.i(TAG, "Post: " + post.getDescription() );
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+
+}
